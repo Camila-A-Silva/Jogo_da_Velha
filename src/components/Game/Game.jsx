0f5cde9b82b34c7953b073c4f929b754cb8ca7a3
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-09-03 17:23:03 -0300

    Adicionei um placar e coloquei as variáveis do css

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,9 +2,25 @@ import { useState } from 'react';
 import Board from '../Board/Board';
 import styles from './Game.module.css';
 
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontais
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticais
+      [0, 4, 8], [2, 4, 6]             // Diagonais
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [scores, setScores] = useState({ x:0, o:0, empate:0});
 
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
@@ -13,6 +29,19 @@ export default function Game() {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
+
+    //Verifica se a jogada acabou e atualiza o placar
+
+    const winner = calculateWinner(nextSquares);
+    const isEmpate = !winner && nextSquares.every((square) => square !== null);
+
+    if (winner === 'X') {
+      setScores((prev) => ({ ...prev, x: prev.x + 1 }));
+    } else if (winner === 'O') {
+      setScores((prev) => ({ ...prev, o: prev.o + 1 }));
+    } else if (isEmpate) {
+      setScores((prev) => ({ ...prev, empate: prev.empate + 1 }));
+    }
   }
 
   function jumpTo(nextMove) {
@@ -23,18 +52,23 @@ export default function Game() {
   }
   }
 
+  function handleResetAll() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+    setScores({ X:0, O:0, Empate:0});
+  }
+
   const moves = history.map((squares, move) => {
-    let voltar;
     let description;
     if (move > 0) {
       description = 'Ir para a jogada #' + move;
     } else {
-      voltar = 'Iníciar jogo';
+      description = 'Iníciar jogo';
     }
     return (
-      <li className={styles.botao} key={move}>
-        <button className={styles.btn} onClick={() => jumpTo(move)}>{voltar}</button>
-        <p>{description}</p>
+
+      <li className={styles.lista}>
+          <button className={styles.btn} onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
@@ -44,9 +78,26 @@ export default function Game() {
       <div className={styles['game-board']}>
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className={styles['game-info']}>
+      
+
+        {/*Placar*/}
+      <div className={styles['tplacar']}>
+        <h2>Placar</h2>
+        <div className={styles['ganhadores']}>
+          <span>X: <strong>{scores.x}</strong></span>
+          <br/>
+          <span>O: <strong>{scores.o}</strong></span>
+          <br/>
+          <span>Empates: <strong>{scores.empate}</strong></span>
+        </div>
+        <button className={styles['btn-reiniciar']} onClick={handleResetAll}>Reiniciar Jogo</button>
+      </div>
+
+      <div className={styles['historico']}>
         <ol className={styles.ol}>{moves}</ol>
       </div>
+        
     </div>
+    
   );
 }
